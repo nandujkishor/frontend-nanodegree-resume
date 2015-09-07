3853ce793b5870ed704e9635cd5c1b3d407cdee8
nandujkishor <nandujkishor@gmail.com>
2015-09-07 22:07:57 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,2 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Nandakishore");
+$('#header').append(formattedName);
